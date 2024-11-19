a4134588edcb9fb46c4beffa56fcea1b9c39cd04
Caio0con <caiohenry18@gmail.com>
2024-11-19 03:01:27 -0300

    projeto finalizado

--- teste/obj/Debug/net8.0/teste.AssemblyInfo.cs
+++ teste/obj/Debug/net8.0/teste.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c72e93f06ec0a36cf8caef375b532dba09eba3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
